f4e42dc640b562d6c3a48c6b6bba476c5eb21bc0
yabdou <yetunde1234@aol.co.uk>
2017-01-31 20:58:37 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+       $( ".wondergirls" ).hide();    
+            $( ".btn1" ).click(function() {
+                $( ".wondergirls" ).slideToggle( "slow" ); 
+            });
+        });
+
+$(document).ready(function(){
+       $( ".stvincent" ).hide();    
+            $( ".btn2" ).click(function() {
+                $( ".stvincent" ).slideToggle( "slow" ); 
+            });
+        });
+
+$(document).ready(function(){
+       $( ".loureed" ).hide();    
+            $( ".btn3" ).click(function() {
+                $( ".loureed" ).slideToggle( "slow" ); 
+            });
+        });
+
+$(document).ready(function(){
+       $( ".phoenix" ).hide();    
+            $( ".btn4" ).click(function() {
+                $( ".phoenix" ).slideToggle( "slow" ); 
+            });
+        });
+		
